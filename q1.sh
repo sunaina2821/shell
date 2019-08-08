44c268b1588bc7faf153fbfa348f73686260a381
Rohan35niser <rohan.naskar@niser.ac.in>
2019-08-08 10:41:56 -0400

    Yay, completed

---
+++ q1.sh
@@ -0,0 +1,9 @@
+read -p "What's your name? " nameee
+echo $nameee
+mkdir $nameee
+cd $nameee
+touch $nameee.cpp
+
+
+
+
---
+++ q2.sh
@@ -0,0 +1,10 @@
+read -p "What is your name? " name4
+echo $name4
+mkdir $name4
+cd $name4
+touch $name4"1".cpp
+touch $name4"2".cpp
+touch $name4"3".cpp
+touch $name4"4".cpp
+touch $name4"5".cpp
+
